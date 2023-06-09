860638e61fef2387585759eba95766c2ca4a4db1
munzertopu <munzertopu@gmail.com>
2023-06-10 01:02:20 +0600

    private route implemented

--- src/auth/PrivateRoute.jsx
+++ src/auth/PrivateRoute.jsx
@@ -2,9 +2,13 @@ import { Navigate } from "react-router-dom";
 
 const useAuth = () => {
   let auth = null;
+  const token = JSON.parse(localStorage.getItem("fake_access_token"));
+  if (token) auth = token;
+  return auth;
 };
-function ProtectedRoute({ auth, redirectPath = "/", children }) {
-  if (!auth) {
+function ProtectedRoute({ redirectPath = "/", children }) {
+  const authenticated = useAuth();
+  if (!authenticated) {
     return <Navigate to={redirectPath} replace />;
   }
   return children;
