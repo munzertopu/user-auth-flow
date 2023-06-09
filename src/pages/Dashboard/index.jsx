db75f4cc2d40e676f393e22a8657eb019da5035b
munzertopu <munzertopu@gmail.com>
2023-06-10 01:31:52 +0600

    protected route implemented in route

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <h1>Click on the Logout button to clear the current session </h1>;
+};
+
+export default Dashboard;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./auth/PrivateRoute";
+import DashboardLayout from "./components/Layouts/DashboardLayout";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 
@@ -8,6 +11,16 @@ const Routers = () => {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="registration" element={<Registration />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 };
